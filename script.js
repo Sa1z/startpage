210a5882dfb0a39288a0ba8b0b341e0634e33620
Sa1z <diaz3737@gmail.com>
2022-01-06 17:06:49 -0500

    Clock added.

---
+++ script.js
@@ -0,0 +1,28 @@
+// This code was written by Aaron Farrar (@afarrar) on codepen.io.
+function showTime(){
+    let date = new Date();
+    let h = date.getHours(); // 0 - 23
+    let m = date.getMinutes(); // 0 - 59
+    let session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    
+    let time = h + ":" + m + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+
+showTime();
